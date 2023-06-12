fe166a5e68fc450682a006e026d7c5bdcca499f6
VaibhavPujari <vaibhavpujari9@gmail.com>
2023-06-12 13:50:42 +0530

    Document Object Model
    Implemention of DOM

---
+++ DOM/domAssign.js
@@ -0,0 +1,10 @@
+
+
+const overviewSelect = document.querySelector("h1");
+console.log(overviewSelect.innerHTML);
+
+const secondAvantage = document.querySelectorAll(".obj");
+console.log(secondAvantage[1].innerHTML);
+
+const adv = document.querySelector("#advantage");
+console.log(adv.innerHTML);
